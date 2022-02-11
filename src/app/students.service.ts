a3d69bdc136e0601cb09046c0f425e3ba36c6c08
Mateusz Zaremba <mat.zar@icloud.com>
2022-02-11 17:08:57 +0100

    added mock students database

--- src/app/students.service.ts
+++ src/app/students.service.ts
@@ -28,9 +28,27 @@ export interface Student {
   status?: 'present' | 'absent';
 }
 
+const mockStudents: Student[] = [
+  { id: '1', firstName: 'Greg', lastName: 'Marine' },
+  { id: '2', firstName: 'Jonathan', lastName: 'Bennett' },
+  { id: '3', firstName: 'Neil', lastName: 'Estandarte' },
+  { id: '4', firstName: 'Jen', lastName: 'Townsend' },
+  { id: '5', firstName: 'Casey', lastName: 'McBride' },
+  { id: '6', firstName: 'Diane', lastName: 'Rivera' },
+  { id: '7', firstName: 'Troy', lastName: 'Gutierrez' },
+  { id: '8', firstName: 'Priscilla', lastName: 'Little' },
+  { id: '9', firstName: 'Bobby', lastName: 'Robbins' },
+  { id: '10', firstName: 'Edmund', lastName: 'Gardner' },
+];
+
 @Injectable({
   providedIn: 'root',
 })
 export class StudentsService {
   constructor() {}
+
+  getAll() {
+    // ... is a shorthand for making a shallow copy of an array.
+    return [...mockStudents];
+  }
 }
