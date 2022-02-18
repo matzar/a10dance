06e6cfd8188ff5a8c4a8b8c55d5ed3e471cee321
Mateusz Zaremba <mat.zar@icloud.com>
2022-02-18 13:55:20 +0100

    added ion-action-sheet on click on the student

--- src/app/roster/roster.page.ts
+++ src/app/roster/roster.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentsService, Student } from '../students.service';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-roster',
@@ -13,9 +14,12 @@ export class RosterPage implements OnInit {
   // page's constructor.
   // Do it, by inserting a new private parameter
   // `studentService`, of type `StudentService.
-  // Marking the parameter private automatically exposes
-  // the parameter as a member of the component class - it is a handy shortcut TypeScript provides.
-  constructor(private studentService: StudentsService) {}
+  // Marking the parameter private or public
+  // automatically exposes the parameter as a member of the component class - it is a handy shortcut TypeScript provides.
+  constructor(
+    public actionSheetController: ActionSheetController,
+    private studentService: StudentsService
+  ) {}
 
   // By default, the Roster Page implements Angular's `OnInit` interface,
   // which requires you to implement the `OnInit` Angular hook.
@@ -30,5 +34,61 @@ export class RosterPage implements OnInit {
   deleteStudent(student): Student[] {
     this.students.splice(this.students.indexOf(student), 1);
     return student;
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albums',
+      cssClass: 'my-custom-class',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash',
+          id: 'delete-button',
+          data: {
+            type: 'delete',
+          },
+          handler: () => {
+            console.log('Delete clicked');
+          },
+        },
+        {
+          text: 'Share',
+          icon: 'share',
+          data: 10,
+          handler: () => {
+            console.log('Share clicked');
+          },
+        },
+        {
+          text: 'Play (open modal)',
+          icon: 'caret-forward-circle',
+          data: 'Data value',
+          handler: () => {
+            console.log('Play clicked');
+          },
+        },
+        {
+          text: 'Favorite',
+          icon: 'heart',
+          handler: () => {
+            console.log('Favorite clicked');
+          },
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          },
+        },
+      ],
+    });
+    await actionSheet.present();
+
+    const { role, data } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role and data', role, data);
   }
 }
