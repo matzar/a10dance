a22c8b108af91e9b1a5d59a9e02cb88180a5efed
Mateusz Zaremba <mat.zar@icloud.com>
2022-02-18 15:57:17 +0100

    ref: removed unnecessary imports

--- src/app/roster/roster.page.ts
+++ src/app/roster/roster.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { StudentsService, Student } from '../students.service';
 import { ActionSheetController, AlertController } from '@ionic/angular';
 
